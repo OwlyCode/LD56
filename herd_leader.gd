adfae7e6334a6e2bb6a4e47183174d5888c643d6
Tristan Maindron <tmaindron@gmail.com>
2024-10-11 15:52:57 +0200

    Added arrows and touchscreen

--- herd_leader.gd
+++ herd_leader.gd
@@ -19,7 +19,7 @@ func _process(delta: float) -> void:
 
 	position.z = max(-len(kopaings) * 0.1, -4.0)
 
-	if Input.is_action_just_pressed("forward") and true:
+	if Input.is_action_just_pressed("forward") and false:
 		for x in range(1):
 			var k = kopeng.instantiate()
 			k.position = position + Vector3.BACK * 1.2;
@@ -29,6 +29,15 @@ func _process(delta: float) -> void:
 			get_node("/root/World").add_child(k)
 	if Input.is_action_pressed("left"):
 		position.x -= speed * delta
+
+	var screen_pos = get_node("/root/World/Camera3D").unproject_position(position)
+
+	if touches > 0:
+		if screen_pos.x > touch_position.x:
+			position.x -= speed * delta
+		elif screen_pos.x < touch_position.x:
+			position.x += speed * delta
+
 	if Input.is_action_pressed("right"):
 		position.x += speed * delta
 
@@ -36,6 +45,28 @@ func _process(delta: float) -> void:
 		position.x = 4.5
 	if position.x < -4.5:
 		position.x = -4.5
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		handle_touch(event)
+	if event is InputEventScreenDrag:
+		handle_drag(event)
+
+@onready var touch_position = position
+
+const TOUCH_DEADZONE = 1
+
+var touches = 0
+
+func handle_touch(event):
+	if event.pressed:
+		touch_position = event.position
+		touches += 1
+	else:
+		touches -= 1
+
+func handle_drag(event):
+	touch_position = event.position
 
 func _physics_process(_delta):
 	var space_state = get_world_3d().direct_space_state
